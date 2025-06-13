c4bfe1acd8e982f8164993282ff6bacaacc7f96e
dibashbikramthapa <dibash.thapa@bitscraft.com>
2025-06-13 08:45:26 +0545

    Day 5
     - [x] Registered todo app in project and admin panel.
     - [x] Override deafult str representation.

--- todo/core/models.py
+++ todo/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Todo(models.Model):
+
+    name = models.fields.CharField(max_length=255)
+    description = models.fields.CharField(max_length=255)
+    created_at = models.fields.DateField(default=datetime.now())
+    deadline = models.fields.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f""" name: {self.name}
+                created_date:{self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"""

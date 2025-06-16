a2247255309749804c0fbf1d0499c23ef92b535e
dibashbikramthapa <dibash.thapa@bitscraft.com>
2025-06-16 19:18:39 +0545

    Add list url with template to list todo datas.

--- todo/core/views.py
+++ todo/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views import generic
 # Create your views here.
+
+from core.models import Todo
+
+
+class TodoListView(generic.ListView):
+    queryset = Todo.objects.all()
+    template_name = 'core/list.html'
+    context_object_name = 'todos'
+
+

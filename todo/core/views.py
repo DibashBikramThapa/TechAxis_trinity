44e5f166da9d92bab666d0d18f0d31ae8dd95789
dibashbikramthapa <dibash.thapa@bitscraft.com>
2025-06-20 08:34:11 +0545

    Search in todo
     - [x] Or in name and descp

--- todo/core/views.py
+++ todo/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
+from django.db.models import Q
 # Create your views here.
 
 from core.models import Todo
@@ -11,6 +12,14 @@ class TodoListView(generic.ListView):
     queryset = Todo.objects.all()
     template_name = 'core/list.html'
     context_object_name = 'todos'
+
+    def get_queryset(self):
+        queryset =  super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            my_filter = Q(name__contains=query) | Q(description__contains=query)
+            queryset = queryset.filter(my_filter)
+        return queryset
 
 
 class TodoCreateView(generic.CreateView):
